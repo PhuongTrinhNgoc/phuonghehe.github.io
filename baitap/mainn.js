8d98a5e5e024bd0cfb4172e6421a7538be63c1ee
phuonghehe <102595122+phuonghehe@users.noreply.github.com>
2022-04-26 21:59:07 +0700

    update

---
+++ baitap/mainn.js
@@ -0,0 +1,3 @@
+var a = 1;
+for (i=1;i<9;i+=2);
+document.write(i+' ')
---
+++ test/mainn.js
@@ -0,0 +1,11 @@
+var myArray = [
+    'fđfdfd',
+    'fd gdkl sfsj',
+    'gj djgfk ldkl',
+    'j g d   klgf  ldgjk',
+    'gf k  dgkfkgfk'
+];
+var arrayLength = myArray.length;
+for (var i = 0;i < arrayLength; i++){
+    document.write(myArray[i]);
+}
