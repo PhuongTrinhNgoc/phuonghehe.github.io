d9e19eb5acf67ed1712077827058bcc00062fd74
phuonghehe <102595122+phuonghehe@users.noreply.github.com>
2022-06-02 18:51:03 +0700

    a

--- Dom_b1/script.js
+++ Dom_b1/script.js
@@ -9,10 +9,6 @@ const fruitList = [
 ];
 const newUl =  document.querySelector("ul");
 
-    result = [];
 for(let i = 0 ; i<fruitList.length;i++){
-result += `<li>${fruitList[i]}</li>`
+    newUl.innerHTML += `<li>${fruitList[i]}</li>`
 }
-
-
-newUl.innerHTML= result;
