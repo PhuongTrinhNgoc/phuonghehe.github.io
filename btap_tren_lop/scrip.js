189ece4b870f4e4ff8e60bf7a3b6425e95d1ee83
phuonghehe <102595122+phuonghehe@users.noreply.github.com>
2022-06-08 20:39:49 +0700

    a

--- btap_tren_lop/scrip.js
+++ btap_tren_lop/scrip.js
@@ -10,19 +10,75 @@
 
 // // API của 
 
-const nodelement = document.querySelector (".para");
+// const nodelement = document.querySelector (".para");
 
-const content = nodelement.innerText;
+// const content = nodelement.innerText;
 
-const words = content.split(" ");
-let count = 0;
-for (let i = 0; i < words.length;i++){
-    const word = words[i];
-    if(word.length >= 8){
-        count++;
-        words[i] = `<mark>${word}</mark>`;
-    }
+// const words = content.split(" ");
+// let count = 0;
+// for (let i = 0; i < words.length;i++){
+//     const word = words[i];
+//     if(word.length >= 8){
+//         count++;
+//         words[i] = `<mark>${word}</mark>`;
+//     }
+// }
+// const newContent = words.join(" ");
+// p.innerHTML = newContent;
+// document.title = count
+function clickButton(){
+    document.getElementById('red').style.display="block";
 }
-const newContent = words.join(" ");
-p.innerHTML = newContent;
-document.title = count
+function listAdd(event){
+// console.log(event.target.value);
+if(event.target.value == 'ha-noi'){
+    document.getElementById('huyen').innerHTML=
+    `<select name="huyen" id="huyen">
+        <option value="hai-ba-trung" >ha noi 1</option>;
+        <option value="abc" >ha noi 2</option>
+    </select>`
+}
+
+if(event.target.value== 'hai-phong'){
+    document.getElementById('huyen').innerHTML=
+`<select name="huyen" id="huyen">
+<option value="hai-phong-1" >hai phong 1</option>;
+<option value="hai-phong-2" >hai phong 2</option>
+</select>`
+}
+}
+
+
+
+
+function listH(event){
+    // console.log(event.target.value);
+    if(event.target.value == 'hai-phong-2'){
+        document.getElementById('quan').innerHTML=
+        `<select name="quan" id="quan">
+        <option value="" >hai phong city-1</option>;
+        <option value="" >hai phong city-2</option>
+    </select>`
+    }
+    if(event.target.value == 'hai-phong-1'){
+        document.getElementById('quan').innerHTML=
+        `<select name="quan" id="quan">
+        <option value="" >ố zề</option>;
+        <option value="" >ối dời ôi</option>
+    </select>`
+    }
+    if(event.target.value == 'hai-ba-trung'){
+        document.getElementById('quan').innerHTML=
+        `<select name="quan" id="quan">
+        <option value="" >hi</option>;
+        <option value="" >hello</option>
+    </select>`
+}
+if(event.target.value == 'abc'){
+    document.getElementById('quan').innerHTML=
+    `<select name="quan" id="quan">
+    <option value="" >ha noi 2 - 2</option>;
+    <option value="" >ha noi 2-3</option>
+</select>`
+}
+}
---
+++ clone2/script.js
@@ -0,0 +1,13 @@
+// let xOverlay = document.querySelector('.overlay')
+// let xOverlay2 = document.querySelector('.nav_overlay')
+// function closeX(){
+//     xOverlay.classList.toggle('overlay');
+//     xOverlay2.classList.toggle('nav_overlay')
+// }
+let xOverlay = document.querySelector('.overlay');
+function onBars(){
+    xOverlay.classList.toggle('overlay');
+}
+function closeX(){
+    xOverlay.classList.toggle('overlay');
+}
